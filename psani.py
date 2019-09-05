d2ea894f32ae0496bf1432db7746c9e6d20bfb13
akitenshi <54932443+akitenshi@users.noreply.github.com>
2019-09-05 09:41:15 +0200

    Add files via upload

---
+++ psani.py
@@ -0,0 +1,18 @@
+# psani
+
+def cls():
+	print('\n' * 50)
+
+dal = "ano"
+
+while dal == "a" or dal == "ano":
+
+	cls()
+
+	vstup = input("\nNapis neco:\n")
+	print("\n[", vstup, "]\n")
+	print("Co to tam pises?")
+
+	dal = input("\nChces psat dal? ")
+
+print("\nTak ses blbej.\n")
